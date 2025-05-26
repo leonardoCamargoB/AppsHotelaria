56c41dd6704fbf02013a92e6edaea191d6422ed7
LEONARDO CAMARGO BENETTE <leonardo.cbenette@senacsp.edu.br>
2025-05-26 17:15:48 -0300

    Controle das variaveis de ambiente para o acesso ao DB

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -1,16 +1,40 @@
 package util;
 
+
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Conexao {
 
-    private String driver = "com.mysql.cj.jdbc.Driver";
-    private String url = "jdbc:mysql://localhost:3306/appHoelaria";
-    private String user = "dbaHotel";
-    private String senha = "dbaHotel123";
+    private String driver;
+    private String url;
+    private String user;
+    private String senha;
 
+    public  Conexao() {
+        carregarConfiguracoes();
+    }
+
+    private void carregarConfiguracoes() {
+        Properties props = new Properties();
+        try (InputStream inputPropsConfig = getClass().getClassLoader().getResourceAsStream
+                ("config.properties")){
+          props.load(inputPropsConfig);
+          driver = props.getProperty("driver");
+          url = props.getProperty("url");
+          user = props.getProperty("user");
+          senha = props.getProperty("senha");
+        }
+        catch (IOException erro) {
+
+            System.out.println("Erro ao carregar configurações" + erro.getMessage());
+        }
+    }
     public Connection conectar() {
         Connection condb = null;
 
